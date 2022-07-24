64bc0c3ce664275017e20d5553adba25c59b623b
Zoe Mendez <zoemendez316@gmail.com>
2022-07-24 13:08:35 -0600

    commit tree de1292137df3046fd01512ca8fdc90121bffd3b6

--- main.js
+++ main.js
@@ -3,6 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likesArray = [...document.getElementsByClassName("like")]
+
+likesArray.forEach((item) => {
+  item.addEventListener('click', () => {
+
+    mimicServerCall()
+    .then((res)=>{
+      const heartSpan = item.children[0]
+
+      if(heartSpan.classList.contains("activated-heart")){
+        heartSpan.classList.remove("activated-heart")
+        heartSpan.textContent =  EMPTY_HEART
+      }
+      else{
+        heartSpan.textContent = FULL_HEART
+        heartSpan.classList.add("activated-heart")
+      }
+    })
+    .catch((err)=>{
+      let errorText = document.getElementById("modal-message")
+      let errorModal = document.getElementById('modal')
+      errorText.textContent = err
+      errorModal.classList.remove("hidden")
+      setTimeout(()=> {
+        errorModal.classList.add("hidden")
+      }, "3000")
+  
+    })
+  })
+})
+
 
 
 
